0bebce33d1d44368af60c8cf257776c2c5202406
Nurgazy <tagaevnurgazy.kg@gmail.com>
2022-10-14 15:30:45 +0300

    add personal program

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    Project №1 <p>GitHup</p> 
+    
+    <p>
+         Programmer №1 <p>Joni</p>
+         <span>created program</span>
+    </p>
+    </div>
+  );
+}
+
+export default App;
